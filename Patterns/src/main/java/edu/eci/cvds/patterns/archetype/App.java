bf05cdd439d0cac73643017fe48336d801709fde
andresserrato2004 <andres.serrato-c@mail.escuelaing.edu.co>
2024-08-30 22:30:01 -0500

    laboratorio terminado

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print("Hello ");
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+        
     }
 }
